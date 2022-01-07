41f6de93a393649f130251c3b41c2adcc4b0e561
Daniel Reyes Barrera <danieldhatsct@gmail.com>
2022-01-07 11:07:22 -0600

    prueba echo

--- forms/contact.php
+++ forms/contact.php
@@ -1,19 +1,19 @@
 <?php
-
+echo"hola"
 if (isset($_POST['submit'])) {
   $name = $_POST['name'];
   $emailFrom = $_POST['email'];
   $subject = $_POST['subject'];
   $message = $_POST['message'];
 
-$emailTo = 'danieldhatsct@gmail.com'
+  $emailTo = 'danieldhatsct@gmail.com'
 
-$headers = "From: My website" . $emailFrom;
-$txt = "You have received an e-mail from".$name.".\n\n".$message;
+  $headers = "From: My website" . $emailFrom;
+  $txt = "You have received an e-mail from".$name.".\n\n".$message;
 
-mail($emailTo, $subject, $txt. $headers);
+  mail($emailTo, $subject, $txt. $headers);
 
-header("Location: index.php?mainsend");
+  header("Location: index.php?mainsend");
 
 }
 ?>
